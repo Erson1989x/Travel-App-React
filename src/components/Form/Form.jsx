9adf2add05374166e301e422d189113b03ea52c7
Erhard Nagy <Erson1989x@gmail.com>
2024-12-08 15:14:53 +0200

    progress

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 export const Form = () => {
-  return (
-    <div className='add-form'>
-      <h3>What do you need for your trip ?</h3>
-    </div>
-  )
-}
+  const [description, setDescription] = useState("");
+  const [quantity, setquantity] = useState(1);
 
-export default Form
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = {
+      description,
+      quantity,
+      packed : false ,
+      id: Date.now(),
+    };
+    console.log(newItem);
+
+    setDescription("");
+    setquantity(1);
+  };
+
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip ?</h3>
+      <select defaultValue={1} value={quantity} onChange={(e) => setquantity(Number(e.target.value))} >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((i) => (
+          <option key={i} value={i}>
+            {i}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="item..." value={description} onChange={(e) => setDescription(e.target.value)} />
+      <button>Add</button>
+    </form>
+  );
+};
+
+export default Form;
